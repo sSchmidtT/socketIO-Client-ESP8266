e45f495651ed192f3a6803f7793ef92e1776fd7b
schmidt.ib3 <desenvolvimento1@grupoib3.com.br>
2020-05-23 04:33:49 -0400

    Enviando configuracao e recebendo eventos criado

--- socketio-client.ino
+++ socketio-client.ino
@@ -1,30 +1,102 @@
+#include <Arduino.h>
+#include <ArduinoJson.h>
 #include <ESP8266WiFi.h>
 #include <SocketIoClient.h>
+#define USE_SERIAL Serial
 
 SocketIoClient socketIO;
+
+//Correção nivel lógico invertido
+#define ON LOW
+#define OFF HIGH
+
+const char* ssid = "TP-Link_4FEF"; //Enter SSID
+const char* password = "22022019"; //Enter Password
+const char* socketio_server_host = "10.1.1.110"; //Enter server adress
+const uint16_t socketio_server_port = 3005; // Enter server port
+
+const char* nameDevice = "arduQuarto";
+const int GPIO_0 = 0;
+
+void event(const char * payload, size_t length) {
+  USE_SERIAL.printf("got message: %s\n", payload);
+}
+
+void eventDisconnect(const char * payload, size_t length) {
+  USE_SERIAL.printf("got disconnect: %s\n", payload);
+  
+}
+
+void eventConnect(const char * payload, size_t length) {
+  USE_SERIAL.println("got connect");
+  
+}
+
+void createEvent(const char * payload, size_t length) {
+  USE_SERIAL.printf("event: %s \n", payload);
+  
+}
+
+void eventCreate(const char * payload, size_t length){
+  USE_SERIAL.printf("got create: %s\n", payload);
+  StaticJsonDocument<200> doc;
+  JsonObject array = doc.to<JsonObject>();
+
+  StaticJsonDocument<200> docDev;
+  JsonObject dev = docDev.to<JsonObject>();
+  dev["name"] = nameDevice;
+  dev["mac"] = WiFi.macAddress();
+
+  StaticJsonDocument<200> docPin;
+  JsonArray pinOuts = docPin.to<JsonArray>();
+  JsonObject pin1 = pinOuts.createNestedObject();
+  pin1["pintype"] = "D";
+  pin1["name"] = "Lampada";
+  pin1["number"] = GPIO_0;
+  pin1["type"] = "output";
+  pin1["active"] = true;
+
+  dev["pinOuts"] = docPin;
+  array["dev"] = docDev;
+  char output[200];
+  //String output;
+  serializeJson(doc, output);
+
+  USE_SERIAL.println(output);
+  socketIO.emit("created", output);
+}
 
 void setup() {
   // put your setup code here, to run once:
 
-  WiFi.begin("TP-Link_4FEF", "22022019");
-  Serial.print("Connecting");
+  USE_SERIAL.begin(115200);
 
-  while(WiFi.status() != WL_CONNECTED){
+  USE_SERIAL.setDebugOutput(true);
+
+  USE_SERIAL.println();
+  USE_SERIAL.println();
+  USE_SERIAL.println();
+
+  WiFi.begin(ssid, password);
+  USE_SERIAL.print("Connecting");
+
+  while (WiFi.status() != WL_CONNECTED) {
     delay(500);
-    Serial.print(".");
+    USE_SERIAL.print(".");
   }
-  Serial.println();
-  Serial.print("Connected, IP address: ");
-  Serial.println(WiFi.localIP());
+  USE_SERIAL.println();
+  USE_SERIAL.print("Connected, IP address: ");
+  USE_SERIAL.println(WiFi.localIP());
 
-  socketIO.begin("10.1.1.110", 3005);
+  pinMode(GPIO_0, OUTPUT);
+  //digitalWrite(GPIO_0,OFF);
 
-  socketIO.on("reply", messageEventHandler);
-
-}
-
-void messageEventHandler(const char* payload, size_t length){
-  Serial.printf("got message: %s \n", payload);
+  socketIO.on("connect", eventConnect);
+  socketIO.on("disconnect", eventDisconnect);
+  socketIO.on("reply", event);
+  socketIO.on("create", eventCreate);
+  socketIO.on("create-event", createEvent);
+  socketIO.begin(socketio_server_host, socketio_server_port);
 }
 
 uint64_t messageTimestamp;
@@ -32,11 +104,11 @@ void loop() {
   // put your main code here, to run repeatedly:
 
   socketIO.loop();
+  //uint64_t now = millis();
+  //if(now - messageTimestamp > 6000){
+  // messageTimestamp = now;
+  //socketIO.emit("message", "\"this is a message from a client\"");
 
-  uint64_t now = millis();
-  if(now - messageTimestamp > 6000){
-    messageTimestamp = now;
-    socketIO.emit("message", "\"this is a message from a client\"");
-  }
+  //}
 
 }
